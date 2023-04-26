9b8ddeb582542cf04c4748ddaed94d3bd85c8762
TomasNagy <nagy.tamas.guitar@gmail.com>
2023-04-26 17:40:25 +0200

    Incorrect user input error handling

--- nested_for.c
+++ nested_for.c
@@ -5,6 +5,11 @@ int main(void)
 {
 	int n = get_int("How many bricks do you want to print? ");
 
+	while(n < 1)
+	{
+		n = get_int("How many bricks do you want to print? ");
+	}
+
 	for(int i = 0; i < n; i++)
 	{
 		for(int j = 0; j < n; j++)
