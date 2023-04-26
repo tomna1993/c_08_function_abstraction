78d1f6afd0d99299301a96c5e9b8a7409edc9394
TomasNagy <nagy.tamas.guitar@gmail.com>
2023-04-26 17:32:06 +0200

    Set common variable for the loop iterations

--- nested_for.c
+++ nested_for.c
@@ -3,9 +3,11 @@
 
 int main(void)
 {
-	for(int i = 0; i < 3; i++)
+	int n = 5;
+
+	for(int i = 0; i < n; i++)
 	{
-		for(int j = 0; j < 3; j++)
+		for(int j = 0; j < n; j++)
 		{
 			printf("#");
 		}
